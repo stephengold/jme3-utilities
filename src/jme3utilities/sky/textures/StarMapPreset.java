db0564afbf2c7d3230bd486cb9eec13f227d12bc
sgold <sgold@sonic.net>
2014-02-15 00:37:09 +0000

    add 16m version of Wiltshire star map

--- src/jme3utilities/sky/textures/StarMapPreset.java
+++ src/jme3utilities/sky/textures/StarMapPreset.java
@@ -36,7 +36,30 @@ enum StarMapPreset {
     // *************************************************************************
     // values
 
-    NORTH_4M, NORTH_16M, SOUTH_4M, SOUTH_16M, WILTSHIRE_4M;
+    /**
+     * stars of the Northern Hemisphere at 2048x2048 resolution
+     */
+    NORTH_4M,
+    /**
+     * stars of the Northern Hemisphere at 4096x4096 resolution
+     */
+    NORTH_16M,
+    /**
+     * stars of the Southern Hemisphere at 2048x2048 resolution
+     */
+    SOUTH_4M,
+    /**
+     * stars of the Southern Hemisphere at 4096x4096 resolution
+     */
+    SOUTH_16M,
+    /**
+     * stars of Wiltshire 10h33m local sidereal time at 2048x2048 resolution
+     */
+    WILTSHIRE_4M,
+    /**
+     * stars of Wiltshire 10h33m local sidereal time at 4096x4096 resolution
+     */
+    WILTSHIRE_16M;
     // *************************************************************************
     // new methods exposed
 
@@ -55,6 +78,8 @@ enum StarMapPreset {
                 return "south_16m";
             case WILTSHIRE_4M:
                 return "wiltshire";
+            case WILTSHIRE_16M:
+                return "wiltshire_16m";
         }
         return "?";
     }
@@ -86,6 +111,7 @@ enum StarMapPreset {
             case SOUTH_16M:
                 return 0f;
             case WILTSHIRE_4M:
+            case WILTSHIRE_16M:
                 /*
                  * At 10h33m, Orion is about to set in the west and the
                  * Pointers of the Big Dipper are near the meridian.
@@ -109,6 +135,7 @@ enum StarMapPreset {
             case SOUTH_16M:
                 return -FastMath.HALF_PI;
             case WILTSHIRE_4M:
+            case WILTSHIRE_16M:
                 /*
                  * Stonehenge
                  */
@@ -132,6 +159,8 @@ enum StarMapPreset {
                 return "16m/southern";
             case WILTSHIRE_4M:
                 return "wiltshire";
+            case WILTSHIRE_16M:
+                return "16m/wiltshire";
         }
         throw new IllegalStateException();
     }
@@ -149,6 +178,7 @@ enum StarMapPreset {
                 return 2048;
             case NORTH_16M:
             case SOUTH_16M:
+            case WILTSHIRE_16M:
                 return 4096;
         }
         throw new IllegalStateException();
