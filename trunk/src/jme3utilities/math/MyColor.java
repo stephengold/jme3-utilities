7a1fa39f22056a57816ac8f47690abd3b3b45e26
sgold@sonic.net <sgold@sonic.net@b55d618f-2072-858d-87aa-2bf687efedf9>
2014-02-23 00:07:35 +0000

    new method for blending colors, to avoid relying on ColorRGBA.interpolate()

---
+++ trunk/src/jme3utilities/math/MyColor.java
@@ -0,0 +1,82 @@
+/*
+ Copyright (c) 2014, Stephen Gold
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Stephen Gold's name may not be used to endorse or promote products
+ derived from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL STEPHEN GOLD BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package jme3utilities.math;
+
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
+import java.util.logging.Logger;
+import jme3utilities.debug.Validate;
+
+/**
+ * Utility methods for RGBA colors.
+ *
+ * @author Stephen Gold <sgold@sonic.net>
+ */
+public class MyColor {
+    // *************************************************************************
+    // constants
+
+    /**
+     * message logger for this class
+     */
+    final private static Logger logger =
+            Logger.getLogger(MyColor.class.getName());
+    // *************************************************************************
+    // constructors
+
+    /**
+     * A private constructor to inhibit instantiation of this class.
+     */
+    private MyColor() {
+    }
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Interpolate linearly between two colors (with no side effect).
+     * <p>
+     * Use instead of ColorRGBA.interpolate(), which gets renamed in jME 3.1.
+     *
+     * @param fraction (&le;1, &ge;0)
+     * @param start first input color (not null, not altered)
+     * @param end second input color (not null, not altered)
+     * @return a new color, start*(1-fraction) + end*fraction
+     */
+    public static ColorRGBA interpolateLinear(float fraction, ColorRGBA start,
+            ColorRGBA end) {
+        Validate.fraction(fraction, "fraction");
+        Validate.nonNull(start, "start");
+        Validate.nonNull(end, "end");
+
+        float r = FastMath.interpolateLinear(fraction, start.r, end.r);
+        float g = FastMath.interpolateLinear(fraction, start.g, end.g);
+        float b = FastMath.interpolateLinear(fraction, start.b, end.b);
+        float a = FastMath.interpolateLinear(fraction, start.a, end.a);
+        ColorRGBA result = new ColorRGBA(r, g, b, a);
+
+        return result;
+    }
+}
