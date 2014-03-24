c9ebb863019d5a63320c3bfb8d4bb937f1cbbede
sgold <sgold@sonic.net>
2014-03-24 00:41:19 +0000

    +CameraControl

---
+++ src/jme3utilities/controls/CameraControl.java
@@ -0,0 +1,121 @@
+/*
+ Copyright (c) 2014, Stephen Gold
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Stephen Gold's name may not be used to endorse or promote products
+ derived from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL STEPHEN GOLD BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package jme3utilities.controls;
+
+import com.jme3.math.Quaternion;
+import com.jme3.math.Vector3f;
+import com.jme3.renderer.Camera;
+import java.util.logging.Logger;
+import jme3utilities.SimpleControl;
+
+/**
+ * A simple control which manages a camera attached to a spatial.
+ * <p>
+ * Each instance is enabled at creation.
+ *
+ * @author Stephen Gold <sgold@sonic.net>
+ */
+public class CameraControl
+        extends SimpleControl {
+    // *************************************************************************
+    // constants
+
+    /**
+     * message logger for this class
+     */
+    final private static Logger logger =
+            Logger.getLogger(CameraControl.class.getName());
+    // *************************************************************************
+    // fields
+    /**
+     * camera managed by this control: set by constructor
+     */
+    final private Camera camera;
+    /**
+     * camera's offset in local coordinates: set by constructor
+     */
+    final private Vector3f offset;
+    /**
+     * camera's up direction in local coordinates: set by constructor
+     */
+    final private Vector3f upDirection;
+    /**
+     * camera's view direction in local coordinates: set by constructor
+     */
+    final private Vector3f viewDirection;
+    // *************************************************************************
+    // constructors
+
+    /**
+     * Instantiate an enabled control for the specified camera.
+     *
+     * @param camera to manage (not null)
+     * @param offset camera's offset in local coordinates (not null)
+     * @param viewDirection camera's view direction in local coordinates (not
+     * null)
+     * @param upDirection camera's up direction in local coordinates (not null)
+     */
+    CameraControl(Camera camera, Vector3f offset, Vector3f viewDirection,
+            Vector3f upDirection) {
+        assert camera != null;
+
+        this.camera = camera;
+        this.offset = offset.clone();
+        this.viewDirection = viewDirection.clone();
+        this.upDirection = upDirection.clone();
+
+        assert isEnabled();
+    }
+    // *************************************************************************
+    // SimpleControl methods
+
+    /**
+     * Update the camera's location and orientation. Invoked when the camera
+     * node's geometric state is about to be updated, once per frame while this
+     * control attached and enabled.
+     *
+     * @param updateInterval time interval between updates (in seconds, &ge;0)
+     */
+    @Override
+    protected void controlUpdate(float updateInterval) {
+        super.controlUpdate(updateInterval);
+        if (spatial == null) {
+            return;
+        }
+        /*
+         * Update the camera location.
+         */
+        Vector3f worldLocation = spatial.localToWorld(offset, null);
+        camera.setLocation(worldLocation);
+        /*
+         * Update the camera orientation.
+         */
+        Quaternion rotation = spatial.getWorldRotation();
+        Vector3f worldViewDirection = rotation.mult(viewDirection);
+        Vector3f worldUpDirection = rotation.mult(upDirection);
+        camera.lookAtDirection(worldViewDirection, worldUpDirection);
+    }
+}
