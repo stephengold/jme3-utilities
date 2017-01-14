dbf1eab1451f1d7ad0357b3ea8bdcfa5328d212a
sgold@sonic.net <>
2017-01-13 17:23:46 -0800

    added 2 methods: areCollinear(), doCoincide(), and projection() improved test cases

--- src/jme3utilities/math/MyVector3f.java
+++ src/jme3utilities/math/MyVector3f.java
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2013-2014, Stephen Gold
+ Copyright (c) 2013-2017, Stephen Gold
  All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
@@ -81,6 +81,48 @@ public class MyVector3f {
     }
 
     /**
+     * Test whether three points are collinear.
+     *
+     * @param point1 location of the first point (not null, unaffected)
+     * @param point2 location of the second point (not null, unaffected)
+     * @param point3 location of the third point (not null, unaffected)
+     * @param tolerance2 used when comparing two points (in squared world units,
+     * &ge;0)
+     * @return true if collinear, otherwise false
+     */
+    public static boolean areCollinear(Vector3f point1, Vector3f point2,
+            Vector3f point3, float tolerance2) {
+        Validate.nonNull(point1, "first point");
+        Validate.nonNull(point2, "second point");
+        Validate.nonNull(point3, "third point");
+        Validate.nonNegative(tolerance2, "tolerance");
+        /*
+         * Shortcut:
+         * If point1 and point3 coincide, then the three points are collinear. 
+         */
+        Vector3f offset3 = point3.subtract(point1);
+        float normSquared3 = offset3.lengthSquared();
+        if (normSquared3 <= tolerance2) {
+            return true;
+        }
+        /*
+         * The long way:
+         * Calculate the projection of offset2 onto offset3.
+         * Don't use Vector3f.project() because it contains a logic bug.
+         */
+        Vector3f offset2 = point2.subtract(point1);
+        float dot23 = offset2.dot(offset3);
+        Vector3f projection = offset3.mult(dot23 / normSquared3);
+        /*
+         * If the projection coincides with offset2, 
+         * then the three points are collinear.
+         */
+        boolean result = doCoincide(projection, offset2, tolerance2);
+
+        return result;
+    }
+
+    /**
      * Compute the azimuth angle of an offset.
      *
      * @param offset difference of world coordinates (not null, not altered)
@@ -121,9 +163,9 @@ public class MyVector3f {
     /**
      * Compute the distance from one location to another.
      *
-     * @param from world coordinates of starting location (not null, unaffected)
-     * @param to world coordinates of ending location (not null, unaffected)
-     * @return distance (in world units, &ge;0)
+     * @param from coordinates of starting location (not null, unaffected)
+     * @param to coordinates of ending location (not null, unaffected)
+     * @return distance (&ge;0)
      */
     public static float distanceFrom(Vector3f from, Vector3f to) {
         Validate.nonNull(from, "vector");
@@ -131,6 +173,28 @@ public class MyVector3f {
         Vector3f offset = to.subtract(from);
         float distance = offset.length();
         return distance;
+    }
+
+    /**
+     * Test whether two points coincide.
+     *
+     * @param point1 coordinates of the first point (not null, unaffected)
+     * @param point2 coordinates of the second point (not null, unaffected)
+     * @param tolerance2 used when comparing points (in squared units, &ge;0)
+     * @return true if they coincide, otherwise false
+     */
+    public static boolean doCoincide(Vector3f point1, Vector3f point2,
+            float tolerance2) {
+        Validate.nonNull(point1, "first point");
+        Validate.nonNull(point2, "second point");
+        Validate.nonNegative(tolerance2, "tolerance");
+
+        float d2 = point1.distanceSquared(point2);
+        if (d2 > tolerance2) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     /**
@@ -151,10 +215,10 @@ public class MyVector3f {
     }
 
     /**
-     * Compute the horizontal of an offset.
+     * Compute the horizontal direction of an offset in world space.
      *
      * @param offset difference of world coordinates (not null, unaffected)
-     * @return new unit vector
+     * @return a new unit vector
      */
     public static VectorXZ horizontalDirection(Vector3f offset) {
         Validate.nonNull(offset, "offset");
@@ -188,6 +252,26 @@ public class MyVector3f {
     }
 
     /**
+     *
+     * Project vector1 onto vector2. Don't use Vector3f.project() for this
+     * because it contains a logic bug which gives the wrong magnitude when
+     * vector2 has length != 1.
+     *
+     * @param vector1 (not null, unaffected)
+     * @param vector2 (not null, length&gt;0, unaffected)
+     * @return a new vector with the same direction as vector2
+     */
+    public static Vector3f projection(Vector3f vector1, Vector3f vector2) {
+        float lengthSquared = vector2.lengthSquared();
+        Validate.positive(lengthSquared, "length squared of vector2");
+
+        float dot = vector1.dot(vector2);
+        Vector3f projection = vector2.mult(dot / lengthSquared);
+
+        return projection;
+    }
+
+    /**
      * Rotate a vector CLOCKWISE about the +Y axis. Note: Used for applying
      * azimuths, which is why its rotation angle convention is non-standard.
      *
@@ -218,11 +302,14 @@ public class MyVector3f {
 
         // vector test cases
         Vector3f[] vectorCases = new Vector3f[]{
+            new Vector3f(3f, 4f, 12f),
+            new Vector3f(2.5f, 4.5f, 11.5f),
             Vector3f.ZERO,
             Vector3f.UNIT_X,
             Vector3f.UNIT_Z
         };
 
+        System.out.println("Testing yRotate():");
         for (Vector3f v : vectorCases) {
             System.out.printf("v = %s%n", v.toString());
             System.out.printf(" yRotate(v, 0) = %s%n",
@@ -236,5 +323,49 @@ public class MyVector3f {
             System.out.println();
         }
         System.out.println();
+
+        System.out.println("Testing projection():");
+        for (Vector3f v : vectorCases) {
+            System.out.printf("v = %s%n", v.toString());
+            for (Vector3f w : vectorCases) {
+                System.out.printf(" w = %s%n", w.toString());
+                if (w.length() != 0f) {
+                    System.out.printf("  v proj w = %s%n",
+                            projection(v, w).toString());
+                }
+                //System.out.printf("             %s%n",
+                //        v.project(w).toString());
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        System.out.println("Testing doCoincide() with tolerance2 = 0.9:");
+        for (Vector3f v : vectorCases) {
+            System.out.printf("v = %s%n", v.toString());
+            for (Vector3f w : vectorCases) {
+                System.out.printf(" w = %s%n", w.toString());
+                if (doCoincide(v, w, 0.9f)) {
+                    System.out.printf("  v coincides with w%n");
+                } else {
+                    System.out.printf("  v does not coincide with w%n");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        System.out.println("Testing areCollinear():");
+        Vector3f p1 = new Vector3f(1f, 2f, 3f);
+        Vector3f p2 = new Vector3f(2f, 0f, 5f);
+        Vector3f p3 = new Vector3f(4f, -4f, 9f);
+        Vector3f p2bad = new Vector3f(2f, 1f, 5f);
+
+        assert areCollinear(p1, p1, p1, 0.01f);
+        assert areCollinear(p1, p2, p3, 0.01f);
+        assert areCollinear(p1, p2, p2, 0.01f);
+        assert !areCollinear(p1, p2bad, p3, 0.01f);
+
+        System.out.println("Success!");
     }
 }
