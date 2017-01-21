cee0da4e56efe7197fe01399bae39a428b344459
sgold@sonic.net <>
2017-01-20 21:10:47 -0800

    add clock demo

---
+++ src/jme3utilities/nifty/test/ClockDemo.java
@@ -0,0 +1,96 @@
+/*
+ Copyright (c) 2017, Stephen Gold
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Stephen Gold's name may not be used to endorse or promote products
+ derived from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL STEPHEN GOLD BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package jme3utilities.nifty.test;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import jme3utilities.Misc;
+import jme3utilities.MyString;
+import jme3utilities.nifty.GuiApplication;
+import jme3utilities.ui.InputMode;
+
+/**
+ * GUI application for testing/demonstrating dynamic Nifty labels. The
+ * application's main entry point is here.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class ClockDemo
+        extends GuiApplication {
+    // *************************************************************************
+    // constants
+
+    /**
+     * message logger for this class
+     */
+    final private static Logger logger =
+            Logger.getLogger(ClockDemo.class.getName());
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Main entry point for the test harness.
+     *
+     * @param unused array of command-line arguments (not null)
+     */
+    public static void main(String[] unused) {
+        /*
+         * Mute the chatty loggers found in some imported packages.
+         */
+        Misc.setLoggingLevels(Level.WARNING);
+        Logger.getLogger("com.jme3.audio.openal.ALAudioRenderer")
+                .setLevel(Level.SEVERE);
+
+        ClockDemo application = new ClockDemo();
+        application.start();
+        /*
+         * ... and onward to TestMultipleScreens.guiInitializeApplication()!
+         */
+    }
+    // *************************************************************************
+    // GuiApplication methods
+
+    /**
+     * Initialize this GUI application.
+     */
+    @Override
+    public void guiInitializeApplication() {
+        /*
+         * Log the jME3-utilities version string.
+         */
+        logger.log(Level.INFO, "jME3-utilities version is {0}",
+                MyString.quote(Misc.getVersionShort()));
+
+        InputMode inputMode = getDefaultInputMode();
+        /*
+         * Create and attach the screen controller.
+         */
+        ClockScreenController clockScreen = new ClockScreenController();
+        clockScreen.setListener(inputMode);
+        boolean success = stateManager.attach(clockScreen);
+        assert success;
+    }
+}
---
+++ src/jme3utilities/nifty/test/ClockScreenController.java
@@ -0,0 +1,97 @@
+/*
+ Copyright (c) 2017, Stephen Gold
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Stephen Gold's name may not be used to endorse or promote products
+ derived from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL STEPHEN GOLD BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package jme3utilities.nifty.test;
+
+import java.util.Calendar;
+import java.util.logging.Logger;
+import jme3utilities.math.MyMath;
+import jme3utilities.nifty.GuiScreenController;
+
+/**
+ * GUI screen controller used by ClockDemo.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class ClockScreenController
+        extends GuiScreenController {
+    // *************************************************************************
+    // constants
+
+    /**
+     * message logger for this class
+     */
+    final private static Logger logger =
+            Logger.getLogger(ClockScreenController.class.getName());
+    // *************************************************************************
+    // constructors
+
+    /**
+     * Instantiate a disabled controller which will be enabled during
+     * initialization.
+     */
+    public ClockScreenController() {
+        super("ClockDemo/clockScreen",
+                "Interface/Nifty/screens/ClockDemo/clockScreen.xml",
+                true);
+    }
+
+    // *************************************************************************
+    // SimpleAppState methods
+    /**
+     * Callback to update this display. (Invoked once per frame.)
+     *
+     * @param elapsedTime time since the previous update (in seconds, &ge;0)
+     */
+    @Override
+    public void update(float elapsedTime) {
+        super.update(elapsedTime);
+
+        if (!isEnabled()) {
+            return;
+        }
+
+        Calendar rightNow = Calendar.getInstance();
+        int hours = rightNow.get(Calendar.HOUR);
+        int minutes = rightNow.get(Calendar.MINUTE);
+        int seconds = rightNow.get(Calendar.SECOND);
+        int millis = rightNow.get(Calendar.MILLISECOND);
+        /*
+         * Update the labels which show status.
+         */
+        String timeString = String.format("%d:%02d:%02d",
+                hours, minutes, seconds);
+        setStatusText("time1", timeString);
+
+        if (MyMath.isOdd(seconds)) {
+            setStatusText("time2", "odd");
+        } else {
+            setStatusText("time2", "even");
+        }
+        double ssm = millis * 1e-3 + seconds + minutes * 60f + hours * 3600f;
+        String text3 = String.format("%.3f", ssm);
+        setStatusText("time3", text3);
+    }
+}
