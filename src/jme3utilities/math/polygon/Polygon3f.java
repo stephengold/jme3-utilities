aab42ee6ffca135cd3ac7943602f1ccd1765fc31
Stephen Gold <sgold@sonic.net>
2017-01-25 23:18:44 -0800

    use double precision for dot products and squared areas/distances

--- src/jme3utilities/math/polygon/Polygon3f.java
+++ src/jme3utilities/math/polygon/Polygon3f.java
@@ -25,7 +25,6 @@
  */
 package jme3utilities.math.polygon;
 
-import com.jme3.math.FastMath;
 import com.jme3.math.Vector3f;
 import java.util.BitSet;
 import java.util.Map;
@@ -48,8 +47,8 @@ public class Polygon3f {
     /**
      * message logger for this class
      */
-    final private static Logger logger =
-            Logger.getLogger(Polygon3f.class.getName());
+    final private static Logger logger = Logger.getLogger(
+            Polygon3f.class.getName());
     // *************************************************************************
     // fields
     /**
@@ -64,6 +63,11 @@ public class Polygon3f {
      */
     private Boolean isPlanar = null;
     /**
+     * cached squared distances between pairs of corners (each &ge;0, allocated
+     * by constructor, each initialized by setSquaredDistance())
+     */
+    final private Double[][] squaredDistances;
+    /**
      * maximum distance for points to coincide (&ge;0, set by constructor)
      */
     final protected float tolerance;
@@ -71,11 +75,6 @@ public class Polygon3f {
      * square of the tolerance (&ge;0, set by constructor)
      */
     final protected float tolerance2;
-    /**
-     * cached squared distances between corners (each &ge;0, allocated by
-     * constructor, each initialized by setSquaredDistance())
-     */
-    final private Float[][] squaredDistances;
     /**
      * the number of corners, which is also the number of sides (&ge;0, set by
      * constructor)
@@ -110,9 +109,9 @@ public class Polygon3f {
          * Allocate array space.
          */
         cornerLocations = new Vector3f[numCorners];
-        squaredDistances = new Float[numCorners][];
+        squaredDistances = new Double[numCorners][];
         for (int cornerIndex = 0; cornerIndex < numCorners; cornerIndex++) {
-            squaredDistances[cornerIndex] = new Float[numCorners];
+            squaredDistances[cornerIndex] = new Double[numCorners];
         }
         /*
          * Copy corner locations.
@@ -192,13 +191,13 @@ public class Polygon3f {
      */
     public int findCorner(Vector3f point) {
         int result = -1;
-        float bestDS = Float.POSITIVE_INFINITY;
+        double bestSD = Double.POSITIVE_INFINITY;
         for (int cornerIndex = 0; cornerIndex < numCorners; cornerIndex++) {
             Vector3f corner = cornerLocations[cornerIndex];
-            float distanceSquared = corner.distanceSquared(point);
-            if (distanceSquared < bestDS) {
+            double squaredDistance = MyVector3f.distanceSquared(corner, point);
+            if (squaredDistance < bestSD) {
                 result = cornerIndex;
-                bestDS = distanceSquared;
+                bestSD = squaredDistance;
             }
         }
 
@@ -217,17 +216,17 @@ public class Polygon3f {
      */
     public int findSide(Vector3f point, Vector3f storeCoordinates) {
         int result = -1;
-        float bestSD = Float.POSITIVE_INFINITY;
+        double leastSD = Double.POSITIVE_INFINITY;
         for (int sideIndex = 0; sideIndex < numCorners; sideIndex++) {
             /*
              * Try the first corner of the current side.
              */
             Vector3f corner = cornerLocations[sideIndex];
             Vector3f offset = point.subtract(corner);
-            float squaredDistance = offset.lengthSquared();
-            if (squaredDistance < bestSD) {
+            double squaredDistance = MyVector3f.lengthSquared(offset);
+            if (squaredDistance < leastSD) {
                 result = sideIndex;
-                bestSD = squaredDistance;
+                leastSD = squaredDistance;
                 if (storeCoordinates != null) {
                     storeCoordinates.set(corner);
                 }
@@ -238,17 +237,19 @@ public class Polygon3f {
             int nextIndex = nextIndex(sideIndex);
             Vector3f corner2 = cornerLocations[nextIndex];
             Vector3f offset2 = corner2.subtract(corner);
-            float dot = offset.dot(offset2);
-            float lengthSquared = squaredDistance(sideIndex, nextIndex);
+            double dot = MyVector3f.dot(offset, offset2);
+            double lengthSquared = squaredDistance(sideIndex, nextIndex);
             if (dot > 0f && dot < lengthSquared) {
                 /* 
                  * The closest point lies on the side.
                  */
-                Vector3f offsetClosest = offset2.mult(dot / lengthSquared);
-                squaredDistance = offsetClosest.distanceSquared(offset);
-                if (squaredDistance < bestSD) {
+                double fraction = dot / lengthSquared;
+                Vector3f offsetClosest = offset2.mult((float) fraction);
+                squaredDistance = MyVector3f.distanceSquared(
+                        offset, offsetClosest);
+                if (squaredDistance < leastSD) {
                     result = sideIndex;
-                    bestSD = squaredDistance;
+                    leastSD = squaredDistance;
                     if (storeCoordinates != null) {
                         storeCoordinates.set(corner);
                         storeCoordinates.addLocal(offsetClosest);
@@ -278,8 +279,8 @@ public class Polygon3f {
         float sum = 0f;
         for (int cornerIndex = 0; cornerIndex < numCorners; cornerIndex++) {
             int next = nextIndex(cornerIndex);
-            float length2 = squaredDistance(cornerIndex, next);
-            float length = FastMath.sqrt(length2);
+            double lengthSquared = squaredDistance(cornerIndex, next);
+            double length = Math.sqrt(lengthSquared);
             sum += length;
         }
 
@@ -302,8 +303,8 @@ public class Polygon3f {
             cornerMap.clear();
         }
 
-        float tol2 = other.tolerance();
-        tol2 = (tolerance2 + tol2 * tol2) / 2f;
+        float tol = other.tolerance();
+        float tol2 = (tolerance2 + tol * tol) / 2f;
 
         boolean result = false;
         for (int otherI = 0; otherI < other.numCorners(); otherI++) {
@@ -417,9 +418,10 @@ public class Polygon3f {
              * Don't use Vector3f.project() because (as of jME 3.0.10) 
              * it contains a logic bug.
              */
-            float fm_dot_fl = fm.dot(fl);
-            float normSquaredFL = squaredDistance(firstIndex, secondIndex);
-            Vector3f projection = fl.mult(fm_dot_fl / normSquaredFL);
+            double fm_dot_fl = MyVector3f.dot(fm, fl);
+            double normSquaredFL = squaredDistance(firstIndex, secondIndex);
+            double fraction = fm_dot_fl / normSquaredFL;
+            Vector3f projection = fl.mult((float) fraction);
             /*
              * If the projection coincides with FM, 
              * then consider the three points to be collinear.
@@ -494,8 +496,8 @@ public class Polygon3f {
         validateIndex(cornerIndex1, "index of first corner");
         validateIndex(cornerIndex2, "index of second corner");
 
-        float d2 = squaredDistance(cornerIndex1, cornerIndex2);
-        if (d2 > tolerance2) {
+        double squaredDistance = squaredDistance(cornerIndex1, cornerIndex2);
+        if (squaredDistance > tolerance2) {
             return false;
         } else {
             return true;
@@ -514,7 +516,7 @@ public class Polygon3f {
     protected int[] largestTriangle(BitSet cornerSet) {
         Validate.nonNull(cornerSet, "corner set");
 
-        float bestSA = -1f;
+        double largestSA = -1f;
         int[] result = null;
 
         for (int i = cornerSet.nextSetBit(0);
@@ -523,9 +525,9 @@ public class Polygon3f {
                     j >= 0; j = cornerSet.nextSetBit(j + 1)) {
                 for (int k = cornerSet.nextSetBit(j + 1);
                         k >= 0; k = cornerSet.nextSetBit(k + 1)) {
-                    float sa = squaredArea(i, j, k);
-                    if (sa > bestSA) {
-                        bestSA = sa;
+                    double sa = squaredArea(i, j, k);
+                    if (sa > largestSA) {
+                        largestSA = sa;
                         if (result == null) {
                             result = new int[3];
                         }
@@ -550,16 +552,16 @@ public class Polygon3f {
     protected int[] mostDistant(BitSet cornerSet) {
         Validate.nonNull(cornerSet, "corner set");
 
-        float bestSD = -1f;
+        double largestSD = -1.0;
         int[] result = null;
 
         for (int i = cornerSet.nextSetBit(0);
                 i >= 0; i = cornerSet.nextSetBit(i + 1)) {
             for (int j = cornerSet.nextSetBit(i + 1);
                     j >= 0; j = cornerSet.nextSetBit(j + 1)) {
-                float sd = squaredDistance(i, j);
-                if (sd > bestSD) {
-                    bestSD = sd;
+                double squaredDistance = squaredDistance(i, j);
+                if (squaredDistance > largestSD) {
+                    largestSD = squaredDistance;
                     if (result == null) {
                         result = new int[2];
                     }
@@ -573,13 +575,17 @@ public class Polygon3f {
     }
 
     /**
-     * Calculate which corner (or side) follows the specified one.
+     * Calculate which corner (or side) follows the specified one. For this to
+     * work, there must be at least one corner.
      *
-     * @param index which corner (or side) (&ge;0, &lt;numCorners)
+     * @param index index of a corner (or side) (&ge;0, &lt;numCorners)
      * @return index of the following corner (or side) (&ge;0, &lt;numCorners)
      */
     protected int nextIndex(int index) {
         validateIndex(index, "index");
+        if (numCorners < 1) {
+            throw new IllegalStateException("no corners");
+        }
 
         int next = (index + 1) % numCorners;
         return next;
@@ -593,7 +599,7 @@ public class Polygon3f {
      * @param indexC index of the third corner (&ge;0, &lt;numCorners)
      * @return area^2 (&ge;0)
      */
-    protected float squaredArea(int indexA, int indexB, int indexC) {
+    protected double squaredArea(int indexA, int indexB, int indexC) {
         validateIndex(indexA, "index of first corner");
         validateIndex(indexB, "index of second corner");
         validateIndex(indexC, "index of third corner");
@@ -621,15 +627,16 @@ public class Polygon3f {
         /*
          * Using AC as the base, calculate the squared height.
          */
-        float abDotAC = ab.dot(ac);
-        float ac2 = squaredDistance(indexA, indexC);
-        Vector3f projABonAC = ac.mult(abDotAC / ac2);
-        float height2 = ab.distanceSquared(projABonAC);
+        double abDotAC = MyVector3f.dot(ab, ac);
+        double ac2 = squaredDistance(indexA, indexC);
+        double fraction = abDotAC / ac2;
+        Vector3f projABonAC = ac.mult((float) fraction);
+        double height2 = MyVector3f.distanceSquared(ab, projABonAC);
         /*
          * Calculate the squared area.  Area = base * height / 2f.
          */
-        float base2 = squaredDistance(indexA, indexC);
-        float area2 = base2 * height2 / 4f;
+        double base2 = squaredDistance(indexA, indexC);
+        double area2 = base2 * height2 / 4f;
 
         return area2;
     }
@@ -639,9 +646,9 @@ public class Polygon3f {
      *
      * @param cornerIndex1 index of the first corner (&ge;0, &lt;numCorners)
      * @param cornerIndex2 index of the second corner (&ge;0, &lt;numCorners)
-     * @return squared distance in
+     * @return squared distance (&ge;0)
      */
-    protected float squaredDistance(int cornerIndex1, int cornerIndex2) {
+    protected double squaredDistance(int cornerIndex1, int cornerIndex2) {
         validateIndex(cornerIndex1, "index of first corner");
         validateIndex(cornerIndex2, "index of second corner");
 
@@ -781,8 +788,9 @@ public class Polygon3f {
         Vector3f offsetJ = cornerJ.subtract(cornerI);
         Vector3f offsetK = cornerK.subtract(cornerI);
         Vector3f crossProduct = offsetJ.cross(offsetK);
-        float normSquared = crossProduct.lengthSquared();
-        if (normSquared == 0f) {
+        double normSquared = MyVector3f.lengthSquared(crossProduct);
+        float norm = (float) Math.sqrt(normSquared);
+        if (norm == 0f) {
             /*
              * Degenerate case:
              * Treat as planar even though the plane is ill-defined.
@@ -791,7 +799,6 @@ public class Polygon3f {
             isPlanar = true;
             return;
         }
-        float norm = FastMath.sqrt(normSquared);
         Vector3f planeNormal = crossProduct.divide(norm);
         float planeConstant = planeNormal.dot(cornerI);
         /*
@@ -819,11 +826,11 @@ public class Polygon3f {
      */
     private void setSquaredDistance(int index1, int index2) {
         if (index1 == index2) {
-            squaredDistances[index1][index2] = 0f;
+            squaredDistances[index1][index2] = 0.0;
         } else {
             Vector3f corner1 = cornerLocations[index1];
             Vector3f corner2 = cornerLocations[index2];
-            float square = corner1.distanceSquared(corner2);
+            double square = MyVector3f.distanceSquared(corner1, corner2);
             squaredDistances[index1][index2] = square;
             squaredDistances[index2][index1] = square;
         }
