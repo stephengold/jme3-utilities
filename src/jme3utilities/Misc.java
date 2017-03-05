0efbc4d1b33a6d1bbfdc942054d6420a91a85f70
Stephen Gold <sgold@sonic.net>
2017-03-05 14:10:27 -0800

    bump rev to 470

--- src/jme3utilities/Misc.java
+++ src/jme3utilities/Misc.java
@@ -146,7 +146,7 @@ public class Misc {
      * @return package name, branch, and revision of this file
      */
     public static String getVersion() {
-        return "jme3-utilities for_jME3.0 $Rev: 469 $";
+        return "jme3-utilities for_jME3.0 $Rev: 470 $";
     }
 
     /**
