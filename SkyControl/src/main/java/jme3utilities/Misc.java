3e7e1207b9290cddc1b3e0deada0f3d365f8364c
Stephen Gold <sgold@sonic.net>
2017-03-13 19:49:43 -0700

    bump version to 0.9.1+4

--- SkyControl/src/main/java/jme3utilities/Misc.java
+++ SkyControl/src/main/java/jme3utilities/Misc.java
@@ -146,7 +146,7 @@ public class Misc {
      * @return package name, branch, and revision of this file
      */
     public static String getVersion() {
-        return "jme3-utilities master $Rev: 0.9.1+3 $";
+        return "jme3-utilities master $Rev: 0.9.1+4 $";
     }
 
     /**
