fff1a2f8f87248903855907d7bc4baa336e5886e
Stephen Gold <sgold@sonic.net>
2018-01-24 21:59:56 -0800

    add copyright notices

--- SkyControl/src/main/java/jme3utilities/sky/GlobeRenderer.java
+++ SkyControl/src/main/java/jme3utilities/sky/GlobeRenderer.java
@@ -1,5 +1,8 @@
 /*
- Redistribution and use in source and binary forms, with or without
+ Copyright (c) 2014-2018, Stephen Gold
+ All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
  * Redistributions of source code must retain the above copyright
  notice, this list of conditions and the following disclaimer.
