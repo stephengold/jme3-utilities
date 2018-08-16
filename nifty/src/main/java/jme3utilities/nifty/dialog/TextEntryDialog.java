0aa72c3149e85b028cf9d544cd83e9c824b822be
Stephen Gold <sgold@sonic.net>
2018-08-15 20:54:07 -0700

    TextEntryDialog: add a commit description

--- nifty/src/main/java/jme3utilities/nifty/dialog/TextEntryDialog.java
+++ nifty/src/main/java/jme3utilities/nifty/dialog/TextEntryDialog.java
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2017, Stephen Gold
+ Copyright (c) 2017-2018, Stephen Gold
  All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
@@ -26,8 +26,10 @@
  */
 package jme3utilities.nifty.dialog;
 
+import de.lessvoid.nifty.controls.Button;
 import de.lessvoid.nifty.elements.Element;
 import java.util.logging.Logger;
+import jme3utilities.Validate;
 
 /**
  * Controller for a text-entry dialog box that accepts arbitrary text.
@@ -43,6 +45,27 @@ public class TextEntryDialog implements DialogController {
      */
     final private static Logger logger
             = Logger.getLogger(TextEntryDialog.class.getName());
+    // *************************************************************************
+    // fields
+
+    /**
+     * description of the commit action (not null, not empty, should fit the
+     * button -- about 8 or 9 characters)
+     */
+    final private String commitDescription;
+    // *************************************************************************
+    // constructors
+
+    /**
+     * Instantiate a controller.
+     *
+     * @param description commit description (not null, not empty)
+     */
+    public TextEntryDialog(String description) {
+        Validate.nonEmpty(description, "description");
+
+        commitDescription = description;
+    }
     // *************************************************************************
     // DialogController methods
 
@@ -67,5 +90,8 @@ public class TextEntryDialog implements DialogController {
      */
     @Override
     public void update(Element dialogElement, float elapsedTime) {
+        Button commitButton
+                = dialogElement.findNiftyControl("#commit", Button.class);
+        commitButton.setText(commitDescription);
     }
 }
