6cd70b38bb667a28c98f7c835ec2938acb1284ae
Stephen Gold <sgold@sonic.net>
2018-09-17 19:37:00 -0700

    make VehicleTuning JmeCloneable and also Savable

--- Minie/src/main/java/com/jme3/bullet/objects/infos/VehicleTuning.java
+++ Minie/src/main/java/com/jme3/bullet/objects/infos/VehicleTuning.java
@@ -31,6 +31,14 @@
  */
 package com.jme3.bullet.objects.infos;
 
+import com.jme3.export.InputCapsule;
+import com.jme3.export.JmeExporter;
+import com.jme3.export.JmeImporter;
+import com.jme3.export.OutputCapsule;
+import com.jme3.export.Savable;
+import com.jme3.util.clone.Cloner;
+import com.jme3.util.clone.JmeCloneable;
+import java.io.IOException;
 import java.util.logging.Logger;
 
 /**
@@ -38,7 +46,7 @@ import java.util.logging.Logger;
  *
  * @author normenhansen
  */
-public class VehicleTuning {
+public class VehicleTuning implements JmeCloneable, Savable {
 
     /**
      * message logger for this class
@@ -46,7 +54,7 @@ public class VehicleTuning {
     final public static Logger logger
             = Logger.getLogger(VehicleTuning.class.getName());
 
-    // TODO privatize all fields, add copy constructor
+    // TODO privatize all fields
     /**
      * suspension stiffness constant (10&rarr;off-road buggy, 50&rarr;sports
      * car, 200&rarr;Formula-1 race car, default=5.88)
@@ -73,4 +81,74 @@ public class VehicleTuning {
      * 10000&rarr;kart racer, default=10.5)
      */
     public float frictionSlip = 10.5f;
+    // *************************************************************************
+    // JmeCloneable methods
+
+    /**
+     * Callback from {@link com.jme3.util.clone.Cloner} to convert this
+     * shallow-cloned object into a deep-cloned one, using the specified cloner
+     * and original to resolve copied fields.
+     *
+     * @param cloner the cloner that's cloning this object (not null)
+     * @param original the control from which this control was shallow-cloned
+     * (unused)
+     */
+    @Override
+    public void cloneFields(Cloner cloner, Object original) {
+    }
+
+    /**
+     * Create a shallow clone for the JME cloner.
+     *
+     * @return a new instance
+     */
+    @Override
+    public VehicleTuning jmeClone() {
+        try {
+            VehicleTuning clone = (VehicleTuning) super.clone();
+            return clone;
+        } catch (CloneNotSupportedException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+    // *************************************************************************
+    // Savable methods
+
+    /**
+     * De-serialize these parameters, for example when loading from a J3O file.
+     *
+     * @param im importer (not null)
+     * @throws IOException from importer
+     */
+    @Override
+    public void read(JmeImporter im) throws IOException {
+        InputCapsule capsule = im.getCapsule(this);
+        suspensionStiffness = capsule.readFloat("suspensionStiffness", 5.88f);
+        suspensionDamping
+                = capsule.readFloat("wheelsDampingRelaxation", 0.88f);
+        suspensionCompression
+                = capsule.readFloat("wheelsDampingCompression", 0.83f);
+        frictionSlip = capsule.readFloat("frictionSlip", 10.5f);
+        maxSuspensionTravelCm
+                = capsule.readFloat("maxSuspensionTravelCm", 500f);
+        maxSuspensionForce = capsule.readFloat("maxSuspensionForce", 6000f);
+    }
+
+    /**
+     * Serialize these parameters, for example when saving to a J3O file.
+     *
+     * @param ex exporter (not null)
+     * @throws IOException from exporter
+     */
+    @Override
+    public void write(JmeExporter ex) throws IOException {
+        OutputCapsule capsule = ex.getCapsule(this);
+        capsule.write(suspensionStiffness, "suspensionStiffness", 5.88f);
+        capsule.write(suspensionDamping, "wheelsDampingRelaxation", 0.88f);
+        capsule.write(suspensionCompression, "wheelsDampingCompression",
+                0.83f);
+        capsule.write(frictionSlip, "frictionSlip", 10.5f);
+        capsule.write(maxSuspensionTravelCm, "maxSuspensionTravelCm", 500f);
+        capsule.write(maxSuspensionForce, "maxSuspensionForce", 6000f);
+    }
 }
