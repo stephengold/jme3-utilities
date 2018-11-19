5d39a7d63c6d9fb765bc3d8e59ebcb5657a31aa3
Stephen Gold <sgold@sonic.net>
2018-11-19 12:04:31 -0800

    NativeMeshUtil: rm the unused getTriangleIndexVertexArray() method

--- Minie/src/main/java/com/jme3/bullet/util/NativeMeshUtil.java
+++ Minie/src/main/java/com/jme3/bullet/util/NativeMeshUtil.java
@@ -31,12 +31,7 @@
  */
 package com.jme3.bullet.util;
 
-import com.jme3.scene.Mesh;
-import com.jme3.scene.VertexBuffer.Type;
-import com.jme3.scene.mesh.IndexBuffer;
-import com.jme3.util.BufferUtils;
 import java.nio.ByteBuffer;
-import java.nio.FloatBuffer;
 import java.util.logging.Logger;
 
 /**
@@ -56,44 +51,6 @@ public class NativeMeshUtil {
      * A private constructor to inhibit instantiation of this class.
      */
     private NativeMeshUtil() {
-    }
-
-    /**
-     * Pass a mesh to Native Bullet.
-     *
-     * @param mesh the JME mesh to pass (not null)
-     * @return the unique identifier of the resulting btTriangleIndexVertexArray
-     * (not 0)
-     */
-    public static long getTriangleIndexVertexArray(Mesh mesh) {
-        ByteBuffer triangleIndexBase
-                = BufferUtils.createByteBuffer(mesh.getTriangleCount() * 3 * 4);
-        ByteBuffer vertexBase
-                = BufferUtils.createByteBuffer(mesh.getVertexCount() * 3 * 4);
-        int numVertices = mesh.getVertexCount();
-        int vertexStride = 12; //3 verts * 4 bytes each
-        int numTriangles = mesh.getTriangleCount();
-        int triangleIndexStride = 12; //3 index entries * 4 bytes each
-
-        IndexBuffer indices = mesh.getIndicesAsList();
-        FloatBuffer vertices = mesh.getFloatBuffer(Type.Position);
-        vertices.rewind();
-
-        int verticesLength = mesh.getVertexCount() * 3;
-        for (int i = 0; i < verticesLength; i++) {
-            float tempFloat = vertices.get();
-            vertexBase.putFloat(tempFloat);
-        }
-
-        int indicesLength = mesh.getTriangleCount() * 3;
-        for (int i = 0; i < indicesLength; i++) {
-            triangleIndexBase.putInt(indices.get(i));
-        }
-        vertices.rewind();
-        vertices.clear();
-
-        return createTriangleIndexVertexArray(triangleIndexBase, vertexBase,
-                numTriangles, numVertices, vertexStride, triangleIndexStride);
     }
 
     /**
