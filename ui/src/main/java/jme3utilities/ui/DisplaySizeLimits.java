7f90e7575a465c48c5466f4dec80c5e672e12a03
Stephen Gold <sgold@sonic.net>
2019-01-13 13:01:33 -0800

    move DisplaySizeLimits from "nifty" subproject to "ui" subproject

--- ui/src/main/java/jme3utilities/ui/DisplaySizeLimits.java
+++ ui/src/main/java/jme3utilities/ui/DisplaySizeLimits.java
@@ -24,7 +24,7 @@
  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package jme3utilities.nifty.displaysettings;
+package jme3utilities.ui;
 
 import java.util.logging.Logger;
 import jme3utilities.Validate;
