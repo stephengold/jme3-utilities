901f8562ef5a0ac16400962e256e4992b42b1dfb
Stephen Gold <sgold@sonic.net>
2019-08-06 15:17:22 -0700

    VectorSet: rm redundant "public" access

--- heart/src/main/java/jme3utilities/math/VectorSet.java
+++ heart/src/main/java/jme3utilities/math/VectorSet.java
@@ -91,7 +91,7 @@ public interface VectorSet {
      *
      * @return the count (&ge;0)
      */
-    public int numVectors();
+    int numVectors();
 
     /**
      * Access a Buffer containing all values in this set. No further add() is
@@ -99,5 +99,5 @@ public interface VectorSet {
      *
      * @return a Buffer, flipped but possibly not rewound
      */
-    public FloatBuffer toBuffer();
+    FloatBuffer toBuffer();
 }
