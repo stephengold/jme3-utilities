916661241592e4360740048e74fc844186801ce3
Stephen Gold <sgold@sonic.net>
2020-01-03 12:55:52 -0800

    move MyMesh.swapVertexData() to Element.swap()

--- heart/src/main/java/jme3utilities/Element.java
+++ heart/src/main/java/jme3utilities/Element.java
@@ -146,4 +146,89 @@ public class Element {
                 throw new UnsupportedOperationException(message);
         }
     }
+
+    /**
+     * Swap all data between 2 elements of a VertexBuffer.
+     *
+     * @param vertexBuffer the VertexBuffer to modify (not null)
+     * @param index1 the index of the first element (&ge;0)
+     * @param index2 the index of the 2nd element (&ge;0)
+     */
+    public static void swap(VertexBuffer vertexBuffer, int index1, int index2) {
+        int numElements = vertexBuffer.getNumElements();
+        Validate.inRange(index1, "index1", 0, numElements - 1);
+        Validate.inRange(index2, "index2", 0, numElements - 1);
+        if (index1 == index2) {
+            return;
+        }
+
+        int numComponents = vertexBuffer.getNumComponents();
+        VertexBuffer.Format format = vertexBuffer.getFormat();
+        if (format == VertexBuffer.Format.Half) {
+            numComponents *= 2;
+        }
+        int startPos1 = numComponents * index1;
+        int startPos2 = numComponents * index2;
+
+        switch (format) {
+            case Byte:
+            case UnsignedByte:
+            case Half:
+                ByteBuffer byteBuf = (ByteBuffer) vertexBuffer.getData();
+                for (int cIndex = 0; cIndex < numComponents; ++cIndex) {
+                    byte b1 = byteBuf.get(startPos1 + cIndex);
+                    byte b2 = byteBuf.get(startPos2 + cIndex);
+                    byteBuf.put(startPos1 + cIndex, b2);
+                    byteBuf.put(startPos2 + cIndex, b1);
+                }
+                break;
+
+            case Short:
+            case UnsignedShort:
+                ShortBuffer shortBuf = (ShortBuffer) vertexBuffer.getData();
+                for (int cIndex = 0; cIndex < numComponents; ++cIndex) {
+                    short s1 = shortBuf.get(startPos1 + cIndex);
+                    short s2 = shortBuf.get(startPos2 + cIndex);
+                    shortBuf.put(startPos1 + cIndex, s2);
+                    shortBuf.put(startPos2 + cIndex, s1);
+                }
+                break;
+
+            case Int:
+            case UnsignedInt:
+                IntBuffer intBuf = (IntBuffer) vertexBuffer.getData();
+                for (int cIndex = 0; cIndex < numComponents; ++cIndex) {
+                    int i1 = intBuf.get(startPos1 + cIndex);
+                    int i2 = intBuf.get(startPos2 + cIndex);
+                    intBuf.put(startPos1 + cIndex, i2);
+                    intBuf.put(startPos2 + cIndex, i1);
+                }
+                break;
+
+            case Double:
+                DoubleBuffer doubleBuffer
+                        = (DoubleBuffer) vertexBuffer.getData();
+                for (int cIndex = 0; cIndex < numComponents; ++cIndex) {
+                    double d1 = doubleBuffer.get(startPos1 + cIndex);
+                    double d2 = doubleBuffer.get(startPos2 + cIndex);
+                    doubleBuffer.put(startPos1 + cIndex, d2);
+                    doubleBuffer.put(startPos2 + cIndex, d1);
+                }
+                break;
+
+            case Float:
+                FloatBuffer floatBuf = (FloatBuffer) vertexBuffer.getData();
+                for (int cIndex = 0; cIndex < numComponents; ++cIndex) {
+                    float f1 = floatBuf.get(startPos1 + cIndex);
+                    float f2 = floatBuf.get(startPos2 + cIndex);
+                    floatBuf.put(startPos1 + cIndex, f2);
+                    floatBuf.put(startPos2 + cIndex, f1);
+                }
+                break;
+
+            default:
+                String message = "Unrecognized buffer format: " + format;
+                throw new UnsupportedOperationException(message);
+        }
+    }
 }
