ef1fab226d7b72afb4b862205660125cd1bbbca5
Stephen Gold <sgold@sonic.net>
2020-01-09 13:08:59 -0800

    MyString: use "%g" format in describe()

--- heart/src/main/java/jme3utilities/MyString.java
+++ heart/src/main/java/jme3utilities/MyString.java
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2013-2019, Stephen Gold
+ Copyright (c) 2013-2020, Stephen Gold
  All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
@@ -174,7 +174,7 @@ public class MyString {
      * @return a description (not null, not empty)
      */
     public static String describe(float fValue) {
-        String raw = String.format("%f", fValue);
+        String raw = String.format("%g", fValue);
         String result = MyString.trimFloat(raw);
 
         assert result != null;
