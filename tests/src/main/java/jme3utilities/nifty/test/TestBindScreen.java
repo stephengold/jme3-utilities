2d7596928efeafe4cf96a432b44558c73e91017a
Stephen Gold <sgold@sonic.net>
2021-07-05 12:46:14 -0700

    TestBindScreen: correct typo in message

--- tests/src/main/java/jme3utilities/nifty/test/TestBindScreen.java
+++ tests/src/main/java/jme3utilities/nifty/test/TestBindScreen.java
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2017-2020, Stephen Gold
+ Copyright (c) 2017-2021, Stephen Gold
  All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
@@ -285,7 +285,7 @@ public class TestBindScreen extends GuiApplication {
                     messageHud.addLine("I have a bad feeling about this.");
                     return;
                 case asFire:
-                    messageHud.addLine("Torpedos away!");
+                    messageHud.addLine("Torpedoes away!");
                     return;
                 case asForward:
                     setWarpFactor(warpFactor + 1);
