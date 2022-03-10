c8e3aed736d5f0ee5bbea9ff26677444d0920ef2
Stephen Gold <sgold@sonic.net>
2022-03-10 09:53:36 -0800

    meld.sh:  repos have moved to the NetBeans 12.6 project folder

--- bash/meld.sh
+++ bash/meld.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-S1=/home/sgold/Git/jmonkeyengine/jme3-niftygui/src/main/java
-D1=/home/sgold/Git/jme3-utilities/nifty/src/main/java
+S1=/home/sgold/NetBeansProjects/jmonkeyengine/jme3-niftygui/src/main/java
+D1=/home/sgold/NetBeansProjects/jme3-utilities/nifty/src/main/java
 
 /usr/bin/meld --diff $S1 $D1
