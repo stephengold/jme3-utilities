a14ef87641371dc1e0cdeaaee5a40980991ece42
stephengold <sgold@sonic.net>
2026-08-27 11:03:33 -0700

    AppChooser:  override the new default renderer

--- tests/src/main/java/jme3utilities/nifty/test/AppChooser.java
+++ tests/src/main/java/jme3utilities/nifty/test/AppChooser.java
@@ -143,6 +143,7 @@ final class AppChooser extends GuiApplication {
         boolean loadDefaults = true;
         AppSettings settings = new AppSettings(loadDefaults);
         settings.setAudioRenderer(null);
+        settings.setRenderer(AppSettings.LWJGL_OPENGL32); // default=ANGLE_GLES3
         settings.setResizable(true);
         settings.setSamples(4); // anti-aliasing
         settings.setTitle(title); // Customize the window's title bar.
